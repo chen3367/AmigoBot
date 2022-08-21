163ce7a9dcae358aa51f7545ddaa6a5a99344487
chen3367 <86390353+chen3367@users.noreply.github.com>
2022-08-21 16:53:47 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Discord Bot
+`!regroup [名字(逗號分開)] [幾組(default=2)]`</br>
+分組
+
+`!roll [幾顆(default=1)]`</br>
+骰骰子
---
+++ bot.py
@@ -0,0 +1,55 @@
+import random
+import os
+import nextcord
+from nextcord import Interaction
+from nextcord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+intents = nextcord.Intents.all()
+intents.members = True
+client = commands.Bot(command_prefix='!', help_command=None, intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.command()
+async def help(ctx):
+    response = []
+    response.append('\n'.join(['`!regroup [名字(逗號分開)] [幾組(default=2)]`', '分組']))
+    response.append('\n'.join(['`!roll [幾顆(default=1)]`', '骰骰子']))
+    await ctx.send('\n\n'.join(response))
+
+@client.command()
+async def regroup(ctx, player_list, number_of_groups: int = 2):
+    # Convert player list from a string into a list
+    player_list = list(filter(None, player_list.split(',')))
+
+    # Shuffle list of students
+    random.shuffle(player_list)
+    
+    # Create groups
+    all_groups = []
+    for index in range(number_of_groups):
+        group = player_list[index::number_of_groups]
+        all_groups.append(group)
+    
+    # Format and display groups
+    response = []
+    for index, group in enumerate(all_groups):
+        response.append(f"Group {index+1}: {' / '.join(group)}")
+
+    await ctx.send('\n'.join(response))
+
+@client.command()
+async def roll(ctx, number: int = 1):
+    dice = [
+        str(random.choice(range(1, 7)))
+        for _ in range(number)
+    ]
+    await ctx.send(', '.join(dice))
+
+client.run(TOKEN)
